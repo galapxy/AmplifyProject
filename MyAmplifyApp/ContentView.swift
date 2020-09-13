83bcb0542fc4735b3b08847985c855016d0dae74
galapxy <galapxy@outlook.jp>
2020-09-14 01:01:56 +0900

    Initial Commit

---
+++ MyAmplifyApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyAmplifyApp
+//
+//  Created by Ryota Honda on 2020/09/14.
+//  Copyright © 2020 Ryota Honda. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
